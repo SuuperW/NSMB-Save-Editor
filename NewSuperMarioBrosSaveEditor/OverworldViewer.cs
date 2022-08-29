ccff613d78e263b01db69d815ca38e6fad803899
SuuperW <SuuperW@gmail.com>
2022-08-28 19:29:29 -0500

    display paths

--- NewSuperMarioBrosSaveEditor/OverworldViewer.cs
+++ NewSuperMarioBrosSaveEditor/OverworldViewer.cs
@@ -15,7 +15,8 @@ namespace NewSuperMarioBrosSaveEditor
 	public partial class OverworldViewer : UserControl
 	{
 		ToolTip ttip;
-		List<Panel> overworldControls = new List<Panel>();
+		List<Panel> nodeControls = new List<Panel>();
+		List<Panel> pathControls = new List<Panel>();
 		int worldId;
 
 		public OverworldViewer()
@@ -26,13 +27,21 @@ namespace NewSuperMarioBrosSaveEditor
 
 		public void LoadOverworld(JObject jObject)
 		{
+			SuspendLayout();
+
 			// Remove previous nodes
-			foreach (Panel p in overworldControls)
+			foreach (Panel p in nodeControls)
 			{
 				p.Dispose();
 				Controls.Remove(p);
 			}
-			overworldControls.Clear();
+			nodeControls.Clear();
+			foreach (Panel p in pathControls)
+			{
+				p.Dispose();
+				Controls.Remove(p);
+			}
+			pathControls.Clear();
 			// Scroll to 0, 0 so that locations that are set later on are correct.
 			AutoScrollPosition = new Point(AutoScrollPosition.X, -AutoScrollPosition.Y);
 
@@ -59,7 +68,7 @@ namespace NewSuperMarioBrosSaveEditor
 				p.Tag = (int)node["idInWorld"];
 				ttip.SetToolTip(p, worldPrefix + (string)node["name"]);
 				p.Parent = this;
-				overworldControls.Add(p);
+				nodeControls.Add(p);
 
 				// The control's AutoScroll property does not support scrolling to negative locations.
 				// So, we must adjust all locations so that they are positive.
@@ -70,9 +79,96 @@ namespace NewSuperMarioBrosSaveEditor
 			}
 
 			const int padding = 20;
-			foreach (Panel p in overworldControls)
+			foreach (Panel p in nodeControls)
 				p.Location = new Point(p.Location.X - minX + padding, p.Location.Y - minY + padding);
 			paddingLbl.Location = new Point(maxX - minX + 2 * padding, maxY - minY + 2 * padding);
+
+			const int lineHalfWidth = 3;
+			foreach (JToken node in nodes)
+			{
+				int nodeId = (int)node["idInWorld"];
+				foreach (JToken connection in node["connections"].Where((c) => !(bool)c["isBackwards"]))
+				{
+					string direction = (string)connection["direction"];
+					int otherNode = (int)connection["destinationNodeId"];
+
+					if (direction == "Pipe") continue;
+					else if (direction == "Up" || direction == "Down")
+					{
+						// Temporarily swap X/Y so we can apply the same logic as right/left
+						Point l = nodeControls[nodeId].Location;
+						nodeControls[nodeId].Location = new Point(l.Y, l.X);
+						l = nodeControls[otherNode].Location;
+						nodeControls[otherNode].Location = new Point(l.Y, l.X);
+					}
+					bool isRight = direction == "Right" || direction == "Down";
+
+					// We may need more than 1, if the path curves.
+					List<Panel> pathsForConnection = new List<Panel>();
+					Func<int, int, int, int, Panel> makeNewPanel = (x, y, w, h) =>
+					{
+						Panel p = new Panel();
+						p.BackColor = Color.Black;
+						p.Tag = (int)connection["pathIdInWorld"];
+						p.Parent = this;
+						ttip.SetToolTip(p, "Path " + p.Tag);
+						pathsForConnection.Add(p);
+
+						if (w < 0) { x += w; w = -w; }
+						if (h < 0) { y += h; h = -h; }
+						p.Location = new Point(x, y);
+						p.Size = new Size(w, h);
+
+						return p;
+					};
+
+					// First (and maybe only) segment
+					int startX = nodeControls[nodeId].Location.X + nodeSize / 2;
+					int startY = nodeControls[nodeId].Location.Y + nodeSize / 2;
+					int length = nodeControls[otherNode].Location.X - nodeControls[nodeId].Location.X;
+					if (length == 0) // 8-Bowser does this!
+						length = nodeSeparation * (isRight ? 2 : -2);
+					makeNewPanel(startX, startY - lineHalfWidth, length, lineHalfWidth * 2);
+					// Second segment?
+					if (nodeControls[nodeId].Location.Y != nodeControls[otherNode].Location.Y)
+					{
+						int height = nodeControls[otherNode].Location.Y - nodeControls[nodeId].Location.Y;
+						makeNewPanel( startX + length - lineHalfWidth, startY, lineHalfWidth * 2, height);
+						// And finally, potentially a third to go to horizontal again
+						// This will be necessary on the path to 8-Bowser.
+						if (nodeControls[nodeId].Location.X + length != nodeControls[otherNode].Location.X)
+						{
+							makeNewPanel(
+								startX + length,
+								startY + height - lineHalfWidth,
+								nodeControls[otherNode].Location.X - (nodeControls[nodeId].Location.X + length),
+								lineHalfWidth * 2
+							);
+						}
+					}
+
+					// Swap X/Y back
+					if (direction == "Up" || direction == "Down")
+					{
+						Point l = nodeControls[nodeId].Location;
+						nodeControls[nodeId].Location = new Point(l.Y, l.X);
+						l = nodeControls[otherNode].Location;
+						nodeControls[otherNode].Location = new Point(l.Y, l.X);
+						// new controls
+						foreach (Panel p in pathsForConnection)
+						{
+							l = p.Location;
+							p.Location = new Point(l.Y, l.X);
+							p.Size = new Size(p.Height, p.Width);
+						}
+					}
+
+					foreach (Panel p in pathsForConnection)
+						pathControls.Add(p);
+				}
+			}
+
+			ResumeLayout();
 		}
 
 		public void ApplySave(SaveFile saveFile)
@@ -80,9 +176,9 @@ namespace NewSuperMarioBrosSaveEditor
 			SuspendLayout();
 
 			int baseId = 0x18 * worldId;
-			for (int i = 1; i < overworldControls.Count; i++)
+			for (int i = 1; i < nodeControls.Count; i++)
 			{
-				Panel p = overworldControls[i];
+				Panel p = nodeControls[i];
 				int flags = saveFile.GetNodeFlags(baseId + i);
 				if ((flags & SaveFile.NodeFlags.Unlocked) == 0)
 					p.BackgroundImage = Properties.Resources.Node_Locked;
