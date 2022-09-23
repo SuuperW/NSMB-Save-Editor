48fb10d5f16287aac0a02bccc7ea8900a5b9fd87
SuuperW <SuuperW@gmail.com>
2022-09-22 22:43:27 -0500

    add test for clearing W11

--- SaveEditorTests/UnitTest1.cs
+++ SaveEditorTests/UnitTest1.cs
@@ -1,5 +1,7 @@
-﻿using System.IO;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+
+using Newtonsoft.Json.Linq;
 
 using NewSuperMarioBrosSaveEditor;
 
@@ -8,9 +10,26 @@ namespace SaveEditorTests
 	[TestClass]
 	public class UnitTest1
 	{
+		private void assert(bool condition) => Assert.IsTrue(condition);
+
 		private MemoryStream ms = new MemoryStream(File.ReadAllBytes("Saves/New.sav"));
 
 		private SaveFile saveFile;
+		private WorldCollection worlds;
+
+		private CompletionAction completeAll = new CompletionAction()
+		{
+			Complete = true,
+			NormalExit = true,
+			SecretExit = true,
+			StarCoins = true,
+		};
+
+		public UnitTest1()
+		{
+			JArray jArray = JArray.Parse(File.ReadAllText("data.json"));
+			worlds = (WorldCollection)jArray;
+		}
 
 		[TestInitialize]
 		public void PreTest()
@@ -22,13 +41,23 @@ namespace SaveEditorTests
 		public void TestSomeInitialUnlocks()
 		{
 			// Some nodes
-			Assert.IsTrue(saveFile.IsNodeCompleted(0, 0));
-			Assert.IsTrue(!saveFile.IsNodeCompleted(0, 1));
+			assert(saveFile.IsNodeCompleted(0, 0));
+			assert(!saveFile.IsNodeCompleted(0, 1));
 			// Some paths
-			Assert.IsTrue(saveFile.IsPathUnlocked(0, 0));
-			Assert.IsTrue(!saveFile.IsPathUnlocked(0, 1));
+			assert(saveFile.IsPathUnlocked(0, 0));
+			assert(!saveFile.IsPathUnlocked(0, 1));
 			// Brand-new file doesn't have world 1 unlocked
-			Assert.IsTrue((saveFile.GetWorldFlags(0) & SaveFile.WorldFlags.AllForUnlocked) == 0);
+			assert((saveFile.GetWorldFlags(0) & SaveFile.WorldFlags.AllForUnlocked) == 0);
+		}
+
+		[TestMethod]
+		public void TestClearingW11()
+		{
+			worlds.PerformNodeAction(saveFile, 0, 1, completeAll);
+			assert(saveFile.IsNodeCompleted(0, 1));
+			assert(saveFile.IsPathUnlocked(0, 1));
+			// Path to purple mushroom
+			assert(!saveFile.IsPathUnlocked(0, 21));
 		}
 	}
 }
